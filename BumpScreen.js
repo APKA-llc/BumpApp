7a5c96147a9c3fcfe35fc93365c66e5655f0c507
Mingkuan Yan <mingkuany@gmail.com>
2023-02-13 10:44:41 -0500

    completed rough draft of the "You've bumped!" screen and added file for the bumped profile pic

--- BumpScreen.js
+++ BumpScreen.js
@@ -11,12 +11,34 @@ const BumpScreen = ({ navigation }) => {
   return (
   <LinearGradient colors={['#C44EEE','#562574']} style={{flex:1}}>
     <SafeAreaView style={styles.container}>
-  
-        <View style={styles.container}>
 
-          <Text>Code Goes Here</Text>
-          <Image style={styles.logoStyle} source={require('./assets/bumplogo.png')}/>
+      <View style={styles.profilepiccontainer}>
+
+        <Image style={styles.profilepic} source={require('./assets/alex.jpg')}/>
+
+        <View style={styles.titlecontainer}>
+          <Text style={styles.title}>You've Bumped!</Text>
         </View>
+
+        <View style={styles.namecontainer}>
+          <Text style={styles.name}>Alex</Text>
+        </View>
+
+      </View>
+
+      <View style={styles.biocontainer}>
+
+        <Text style={styles.bio}>Enjoyer of photography, punk rock (playboy carti), and rowing. My question to you: do you got that boom boom pow?</Text>
+
+        <Text style={styles.bioprompt}>I geek out on</Text>
+
+        <Text style={styles.bioresponse}>Cookout lore.</Text>
+
+        <Text style={styles.bioprompt}>My most irrational fear</Text>
+
+        <Text style={styles.bioresponse}>The inevitable heat death of the universe</Text>
+
+      </View>
       
     </SafeAreaView>
   </LinearGradient>
@@ -26,21 +48,62 @@ const BumpScreen = ({ navigation }) => {
 //Styles Sheet Please try to Label Descriptively,
 const styles = StyleSheet.create({
   container: {
-    backgroundColor:'transparent',
+    backgroundColor: 'transparent',
     flex: 1,
-    justifyContent: 'center',
     padding: 20,
   },
-  logoStyle: {
-    justifyContent: 'center', 
+  profilepiccontainer: {
+    width: '100%',
+    flex: 5,
     alignItems: 'center',
-    height:400,
-    width:400,
-    flex:0.8,
-    bottom:40,
-    alignSelf:'center',
-    resizeMode:'contain'
   },
+  profilepic: {
+    width: '100%',
+    height: '100%',
+    position: 'relative',
+  },
+  titlecontainer: {
+    position: 'absolute',
+    width: '80%',
+    marginTop: '3%',
+  },
+  title: {
+    fontSize: 52,
+    fontWeight: 'bold',
+    color: '#fd4d4d',
+    textAlign: 'center',
+  },
+  namecontainer: {
+    position: 'absolute',
+    left: 10,
+    bottom: 10,
+  },
+  name: {
+    color: 'white',
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  biocontainer: {
+    flex: 2,
+    padding: '3%',
+  },
+
+  bio: {
+    color: 'white',
+    fontStyle: 'italic',
+    fontSize: 20,
+  },
+  bioresponse: {
+    marginTop: '1%',
+    color: 'white',
+    fontStyle: 'italic',
+    fontSize: 16,
+  },
+  bioprompt: {
+    marginTop: '5%',
+    color: 'white',
+    fontSize: 18,
+  }
 });
 
 export default BumpScreen;
