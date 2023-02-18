051fb55724c319f899b5ef7fde9ef6ab208deb51
Mingkuan Yan <mingkuany@gmail.com>
2023-02-18 03:29:21 -0500

    updated hinge questions page to reflect new specifications.

--- ProfileScreen.js
+++ ProfileScreen.js
@@ -35,7 +35,7 @@ const ProfileScreen = () => {
   };
 
   const formComplete = () => {
-    if (currentGroup === 5) {
+    if (currentGroup === 7) {
       navigation.navigate('HomeScreen');
     } else {
       setCurrentGroup(currentGroup + 1);
@@ -51,23 +51,30 @@ const ProfileScreen = () => {
   };
 
   let messageTop;
-  if (currentGroup === 6) {
+  let messageBottom;
+  if (currentGroup === 7) {
     messageTop = 'Finish';
+    messageBottom = 'Revise';
   } else {
     messageTop = 'Continue';
+    messageBottom = 'Return';
   }
 
-  let messageBottom;
-  messageBottom = 'Return';
+  const numSelected = 0;
+
+  const [hingeSelected, setHingeSelected] = useState(false);
+  const toggleHingePrompt = () => {
+    setHingeSelected(!hingeSelected);
+  }
 
   return (
       <SafeAreaView style={styles.container}>
         <View style={styles.container}>
 
-          {currentGroup === 1 && (
+          {currentGroup === 1 && ( // enter first name
             <View style = {styles.vanish}>
-              <View style={styles.TitleContainer}>
-                <Text style={styles.Title}>Enter Your Name</Text>
+              <View style={styles.titleContainer}>
+                <Text style={styles.title}>Enter Your Name</Text>
               </View>
 
               <View style={styles.inputContainer}>
@@ -81,10 +88,10 @@ const ProfileScreen = () => {
             </View>
           )}
 
-          {currentGroup === 2 && (
+          {currentGroup === 2 && ( // choose age
             <View style = {styles.vanish}>
-              <View style={styles.TitleContainer}>
-                <Text style={styles.Title}>What is your age?</Text>
+              <View style={styles.titleContainer}>
+                <Text style={styles.title}>What is your age?</Text>
               </View>
 
               <View style={styles.inputContainer}>
@@ -99,10 +106,10 @@ const ProfileScreen = () => {
             </View>
           )}
 
-          {currentGroup === 3 && (
+          {currentGroup === 3 && ( // pick profile pic
            <View style = {styles.vanish}>
-            <View style={styles.TitleContainer}>
-              <Text style={styles.Title}>Choose a photo</Text>
+            <View style={styles.titleContainer}>
+              <Text style={styles.title}>Choose a photo</Text>
             </View>
 
             <View style={styles.inputContainer}>
@@ -115,11 +122,11 @@ const ProfileScreen = () => {
 
           </View>
           )}
-          {currentGroup === 4 && (
+          {currentGroup === 4 && ( // write your introduction/bio
            <View style = {styles.vanish}>
             <TouchableWithoutFeedback onPress={dismissKeyboard}>
-              <View style={styles.TitleContainer}>
-              <Text style={styles.Title}>Introduce Yourself</Text>
+              <View style={styles.titleContainer}>
+              <Text style={styles.title}>Introduce Yourself</Text>
               </View>
             </TouchableWithoutFeedback>
 
@@ -129,15 +136,69 @@ const ProfileScreen = () => {
                 style={styles.bioInput}
                 multiline
                 numberOfLines={5}
+                placeholder="Write Your Bio"
               />
             </View>
           </View>
           )}
 
-          {currentGroup === 5 && (
+          {currentGroup === 5 && ( // choose hinge questions
+            <View style = {styles.hingePromptPageContainer}>
+              <View style={styles.titleContainer}>
+                <Text style={styles.title}>Choose Prompts</Text>
+              </View>
+
+              <View style={styles.promptScrollView}>
+                <ScrollView showsVerticalScrollIndicator={false}>
+
+                  <TouchableOpacity style={[styles.promptContainer, hingeSelected ? styles.promptSelected : styles.promptUnselected]} onPress={toggleHingePrompt}>
+                    <Text style={[styles.hingeQuestion, hingeSelected ? styles.hingeSelected : styles.hingeUnselected]}>Most British Thing</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity style={[styles.promptContainer, hingeSelected ? styles.promptSelected : styles.promptUnselected]} onPress={toggleHingePrompt}>
+                    <Text style={[styles.hingeQuestion, hingeSelected ? styles.hingeSelected : styles.hingeUnselected]}>I'm a Hardcore</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity style={[styles.promptContainer, hingeSelected ? styles.promptSelected : styles.promptUnselected]} onPress={toggleHingePrompt}>
+                    <Text style={[styles.hingeQuestion, hingeSelected ? styles.hingeSelected : styles.hingeUnselected]}>Fan of</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity style={[styles.promptContainer, hingeSelected ? styles.promptSelected : styles.promptUnselected]} onPress={toggleHingePrompt}>
+                    <Text style={[styles.hingeQuestion, hingeSelected ? styles.hingeSelected : styles.hingeUnselected]}>Secret Talent</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity style={[styles.promptContainer, hingeSelected ? styles.promptSelected : styles.promptUnselected]} onPress={toggleHingePrompt}>
+                    <Text style={[styles.hingeQuestion, hingeSelected ? styles.hingeSelected : styles.hingeUnselected]}>Dying Wish</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity style={[styles.promptContainer, hingeSelected ? styles.promptSelected : styles.promptUnselected]} onPress={toggleHingePrompt}>
+                    <Text style={[styles.hingeQuestion, hingeSelected ? styles.hingeSelected : styles.hingeUnselected]}>Most Irrational Fear</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity style={[styles.promptContainer, hingeSelected ? styles.promptSelected : styles.promptUnselected]} onPress={toggleHingePrompt}>
+                    <Text style={[styles.hingeQuestion, hingeSelected ? styles.hingeSelected : styles.hingeUnselected]}>Never-ending Nightmare</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity style={[styles.promptContainer, hingeSelected ? styles.promptSelected : styles.promptUnselected]} onPress={toggleHingePrompt}>
+                    <Text style={[styles.hingeQuestion, hingeSelected ? styles.hingeSelected : styles.hingeUnselected]}>At a Party</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity style={[styles.promptContainer, hingeSelected ? styles.promptSelected : styles.promptUnselected]} onPress={toggleHingePrompt}>
+                    <Text style={[styles.hingeQuestion, hingeSelected ? styles.hingeSelected : styles.hingeUnselected]}>Geek Out</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity style={[styles.promptContainer, hingeSelected ? styles.promptSelected : styles.promptUnselected]} onPress={toggleHingePrompt}>
+                    <Text style={[styles.hingeQuestion, hingeSelected ? styles.hingeSelected : styles.hingeUnselected]}>Cheddar News</Text>
+                  </TouchableOpacity>
+                </ScrollView>
+              </View>
+            </View>
+          )}
+
+          {currentGroup === 6 && ( // write hinge responses
            <View style = {styles.vanish}>
-              <View style={styles.TitleContainer}>
-              <Text style={styles.Title}>Questions</Text>
+              <View style={styles.titleContainer}>
+              <Text style={styles.title}>Answer Prompts</Text>
               </View>
 
             <View style={styles.inputContainer}>
@@ -157,14 +218,31 @@ const ProfileScreen = () => {
           </View>
           )}
 
-            <View style = {styles.buttonContainer}>
-              <TouchableOpacity style={styles.buttonStyle} onPress={formComplete}>
-                <Text style={styles.buttonText}>{messageTop}</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.buttonStyle} onPress={formEscape}>
-                <Text style={styles.buttonText}>{messageBottom}</Text>
-              </TouchableOpacity>
-            </View>
+          {currentGroup === 7 && ( // preview profile --- show MyProfileScreen here (not sure how to do that yet)
+          <View style = {styles.vanish}>
+              <View style={styles.titleContainer}>
+              <Text style={styles.title}>Preview Profile</Text>
+              </View>
+          </View>
+          )}
+
+          <View style = {styles.buttonContainer}>
+            {currentGroup === 5 && (
+              <View style={styles.promptCard}>
+                <View style={styles.fullPromptContainer}>
+                  <Text style={styles.fullPromptText}>If you see me at a party, I'm the one...</Text>
+                </View>
+                <Text style={styles.fullPromptSubtitle}>Full Prompt</Text>
+                <Text style={styles.promptSelection}>{numSelected}/3 SELECTED</Text>
+              </View>
+            )}
+            <TouchableOpacity style={styles.buttonStyle} onPress={formComplete}>
+              <Text style={styles.buttonText}>{messageTop}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonStyle} onPress={formEscape}>
+              <Text style={styles.buttonText}>{messageBottom}</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </SafeAreaView>
  
@@ -182,16 +260,14 @@ const styles = StyleSheet.create({
   vanish:{
     //backgroundColor:'green',
     flex:2
-    
   },
-  TitleContainer:{
+  titleContainer:{
     //backgroundColor:"blue",
     flex:1,
     justifyContent:"flex-end",
     alignItems:'center',
-    
   },
-  Title: {
+  title: {
     fontSize: '45%',
     textAlign: 'center',
     fontWeight: 'bold',
@@ -230,16 +306,14 @@ const styles = StyleSheet.create({
   },
   buttonContainer:{
     //backgroundColor:'red',
-    flex:1,
-    justifyContent:"flex-end"
-
+    flex: 1,
+    justifyContent: "flex-end",
   },
   buttonStyle:{
     backgroundColor: purpleStandard,
     borderRadius: 25,
     padding: 10,
     marginVertical:"1%"
-   
   },
   buttonText:{
     color: "white",
@@ -254,8 +328,29 @@ const styles = StyleSheet.create({
     marginVertical:"1%"
   },
   hingeQuestion:{
-    fontSize:'25%',
-    marginBottom:'3%'
+    fontSize:'18%',
+    fontWeight: '500',
+  },
+  hingeSelected: {
+    color: 'white',
+  },
+  hingeUnselected: {
+    color: purpleStandard,
+  },
+  promptContainer: {
+    alignItems: 'center',
+    width: '100%',
+    padding: 12,
+    borderWidth: 3,
+    borderColor: purpleStandard,
+    borderRadius: '24%',
+    marginBottom: 24,
+  },
+  promptSelected: {
+    backgroundColor: purpleStandard,
+  },
+  promptUnselected: {
+    backgroundColor: 'white',
   },
   hingeInput:{
     borderWidth: 2,
@@ -266,6 +361,48 @@ const styles = StyleSheet.create({
     padding:'2%',
     width:'50%',
     marginBottom:"5%"
+  },
+
+  hingePromptPageContainer: {
+    flex: 2,
+    alignItems: 'center',
+  },
+  promptScrollView: {
+    flex: 5,
+    width: '90%',
+  },
+
+  promptCard: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    borderTopColor: purpleStandard,
+    borderTopWidth: 2,
+  },
+  fullPromptContainer: {
+    borderWidth: 3,
+    borderRadius: 20,
+    borderColor: '#9e9e9e',
+    width: '96%',
+    paddingHorizontal: '5%',
+    paddingVertical: '8%',
+    alignItems: 'center',
+    marginTop: '2%',
+  },
+  fullPromptText: {
+    color: purpleStandard,
+    fontSize: 18,
+    fontWeight: '700',
+  },
+  fullPromptSubtitle: {
+    color: '#9e9e9e',
+    marginVertical: '3%',
+  },
+  promptSelection: {
+    color: '#9e9e9e',
+    fontWeight: '700',
+    fontSize: '16%',
+    flex: 1,
   }
 });
 
